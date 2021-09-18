728b85f31d79c8d792641155985dff07edf698a4
Franky Romero <faromero@stanford.edu>
2021-09-18 05:06:34 +0000

    Refactor and create trace manager
    Traces are now mapped to range for invocation rate

--- examples/benchmark_invoker/invoker.py
+++ examples/benchmark_invoker/invoker.py
@@ -1,10 +1,9 @@
 import os
 import sys
-import glob
 import grpc
 import json
 import argparse
-import pandas as pd
+from trace_manager import TraceQuery, TraceManager
 
 import helloworld_pb2
 import helloworld_pb2_grpc
@@ -14,7 +13,7 @@ endpoint_file = 'benchmark_endpoints.txt'
 
 # Limit traces to particular day(s) (between 1 to 14)
 # If set to empty list, will process all
-filter_days = [1]
+filter_days = [1,2]
 
 def getArgs():
   parser = argparse.ArgumentParser()
@@ -24,6 +23,15 @@ def getArgs():
   parser.add_argument('--tracedir', '-d', type=str, required=False,
                       dest='tracedir', default=None,
                       help='Path to traces directory. If not specified, will use manually-inputted parameters.')
+  parser.add_argument('--minrange', '-q', type=int, required=False,
+                      dest='minrange', default=1,
+                      help='Minimum range to map traces to (Default: 1)')
+  parser.add_argument('--maxrange', '-r', type=int, required=False,
+                      dest='maxrange', default=10,
+                      help='Maximum range to map traces to (Default: 10)')
+  parser.add_argument('--numsec', '-s', type=int, required=False,
+                      dest='numsec', default=5,
+                      help='Number of seconds to simulate. -1 for all (Default: 5)')
   parser.add_argument('--executiontime', '-t', type=int, required=False,
                       dest='executiontime', default=0,
                       help='Execution time in ms (Default: 0 for skip)')
@@ -45,50 +53,6 @@ def readEndpoints():
 
   return endpoints_list
 
-def parseTraces(tracedir):
-  # Helper function to check whether a trace should be parsed
-  def shouldParse(trace_name):
-    if not filter_days:
-      return True
-    for d in filter_days:
-      curr_day = 'd%02d' % d
-      if curr_day in trace_name:
-        return True
-    return False
-
-  all_traces = glob.glob(os.path.join(tracedir, '*.csv'))
-
-  # Key: Day (in the form dXX); Value: DataFrame
-  invocations_map = {}
-  memory_map = {}
-  duration_map = {}
-  for at in all_traces:
-    # Check whether we should parse the next trace
-    if not shouldParse(at):
-      continue
-
-    print('Now parsing:', os.path.basename(at))
-
-    day = at.split('.')[-2]
-
-    # Read into DF
-    df = pd.read_csv(at)
-
-    if 'app_memory' in at:
-      # Keep columns: HashApp, AverageAllocatedMb_pct99
-      df = df[['HashApp','AverageAllocatedMb_pct99']]
-      memory_map[day] = df
-    elif 'function_durations' in at:
-      # Keep columns: HashApp, HashFunction, percentile_Average_99
-      df = df[['HashApp', 'HashFunction', 'percentile_Average_99']]
-      duration_map[day] = df
-    elif 'invocations_per_function' in at:
-      # (Easier to drop.) Drop columns: HashOwner, Trigger
-      df = df.drop(['HashOwner', 'Trigger'], axis=1)
-      invocations_map[day] = df
-
-  return invocations_map, memory_map, duration_map
-
 # Set any of executiontime, objectsize, or memoryallocate to 0 to skip
 def queryFunction(endpoint, executiontime, objectsize, memoryallocate):
   with grpc.insecure_channel(endpoint) as channel:
@@ -107,52 +71,33 @@ def queryFunction(endpoint, executiontime, objectsize, memoryallocate):
     input_str = json.dumps(inputjson)
     print('Querying for: %s' % input_str)
     response = stub.SayHello(helloworld_pb2.HelloRequest(name=input_str))
-    print(response)
 
-def runExperiment(endpoints, invocations_map, memory_map, duration_map):
-  # Helper function to map functions to endpoints, and map functions to app hash
-  # The latter is to speed up searches for memory
-  def mapEndpoints(endpoints, duration_map):
-    endpoint_map = {}
-    func_app_map = {}
-    for _, v in duration_map.items():
-      for _, row in v.iterrows():
-        hash_app = row['HashApp']
-        hash_function = row['HashFunction']
-        if len(endpoints) > 0:
-          next_endpoint = endpoints[0]
-          endpoints.pop(0)
-          endpoint_map[hash_function] = next_endpoint
-          func_app_map[hash_function] = hash_app
-        else:
-          break
-      if len(endpoints) == 0:
-        break
+    return response
 
-    return endpoint_map, func_app_map
+def runExperiment(queries_to_run, num_sec):
+  # If num_sec is negative, determine the number of queries to run
+  if num_sec < 0:
+    num_sec = len(queries_to_run[0].invocations)
 
-  endpoint_map, func_app_map = mapEndpoints(endpoints, duration_map)
+  for i in range(num_sec):
+    for q in queries_to_run:
+      num_invoc = q.invocations[i]
+      # TODO: invoke in parallel
+      queryFunction(q.endpoint, q.execution_time, q.object_size, q.memory)
 
-  for k,v in endpoint_map.items():
-    function_name = k
-    function_app = func_app_map[function_name]
-    endpoint = v
-    # TODO: day is hardcoded
-    df_exec = duration_map['d01']
-    df_mem = memory_map['d01']
-    execution_time = df_exec.loc[df_exec.HashFunction == function_name]['percentile_Average_99'].iloc[0]
-    memory = df_mem.loc[df_mem.HashApp == function_app]['AverageAllocatedMb_pct99'].iloc[0]
-
-    queryFunction(endpoint, execution_time, 0, memory)
   return
 
 def main(args):
   tracedir = args.tracedir
+  min_range = args.minrange
+  max_range = args.maxrange
+  num_sec = args.numsec
 
   if tracedir is not None:
     endpoints = readEndpoints()
-    invocations_map, memory_map, duration_map = parseTraces(tracedir)
-    runExperiment(endpoints, invocations_map, memory_map, duration_map)
+    trace_manager = TraceManager(endpoints, tracedir, filter_days)
+    queries_to_run = trace_manager.generateQueries(min_range, max_range)
+    runExperiment(queries_to_run, num_sec)
   else:
     endpoint = args.endpoint
     executiontime = args.executiontime
---
+++ examples/benchmark_invoker/trace_manager.py
@@ -0,0 +1,152 @@
+import os
+import sys
+import glob
+import numpy as np
+import pandas as pd
+from typing import NamedTuple
+
+class TraceQuery(NamedTuple):
+  endpoint: str
+  execution_time: int
+  memory: int
+  object_size: int
+  invocations: np.ndarray
+
+class TraceManager(object):
+  def __init__(self, endpoints, tracedir, filter_days):
+    self.endpoints = endpoints
+    self.filter_days = filter_days
+
+    invocations_list, memory_list, duration_list = self.__parseTraces(tracedir)
+    self.invocations_list = invocations_list
+    self.memory_list = memory_list
+    self.duration_list = duration_list
+
+  def generateQueries(self, min_range, max_range):
+    endpoint_map, func_app_map = self.__mapEndpoints()
+    invoc_df = self.__mapInvocations(list(endpoint_map.keys()), min_range, max_range)
+    query_list = self.__generateQueryInputs(endpoint_map, func_app_map, invoc_df)
+
+    return query_list
+
+  # Function to generate query inputs
+  def __generateQueryInputs(self, endpoint_map, func_app_map, invoc_df):
+    query_list = []
+
+    for k,v in endpoint_map.items():
+      function_name = k
+      function_app = func_app_map[function_name]
+      endpoint = v
+
+      # TODO: only considering the latency and memory of the first available day.
+      # Other option is to average/99% across all days
+      df_exec = self.duration_list[0]
+      df_mem = self.memory_list[0]
+      execution_time = df_exec.loc[df_exec.HashFunction == function_name]['percentile_Average_99'].iloc[0]
+      memory = df_mem.loc[df_mem.HashApp == function_app]['AverageAllocatedMb_pct99'].iloc[0]
+
+      # Extract pertinent row from invoc_df
+      invoc_row = invoc_df[(invoc_df.HashFunction == function_name)].drop(['HashApp', 'HashFunction'], axis=1)
+      invoc_row_np = invoc_row.to_numpy()[0]
+
+      # Create the next query info
+      next_query_info = TraceQuery(endpoint=endpoint, execution_time=execution_time,
+                                   memory=memory, object_size=0, invocations=invoc_row_np)
+      query_list.append(next_query_info)
+
+    return query_list
+
+  # Function to map the invocations to a range
+  def __mapInvocations(self, function_names, min_range, max_range):
+    joined_df = None
+    for df in self.invocations_list:
+      # Extract relevant rows
+      relevant_df = df[df.HashFunction.isin(function_names)]
+
+      if joined_df is None:
+        joined_df = relevant_df
+      else:
+        joined_df = joined_df.merge(relevant_df, how='left', on=['HashApp', 'HashFunction'])
+
+    # Find the max and min of the DF
+    max_val = joined_df.select_dtypes(include=[np.number]).max().max()
+    min_val = joined_df.select_dtypes(include=[np.number]).min().min()
+
+    # Remap all values to desired range
+    input_range = max_val - min_val
+    output_range = max_range - min_range
+
+    def newRange(inp):
+      # Ignore if dtype is not int64
+      # This allows us to keep the HashApp and HashFunction columns intact
+      if inp.dtype != np.int64:
+        return inp
+
+      return np.round((inp - min_val)*output_range / input_range + min_range)
+
+    joined_df = joined_df.apply(newRange)
+
+    return joined_df
+
+  # Function to map functions to endpoints, and map functions to app hash
+  # The latter is to speed up searches for memory
+  def __mapEndpoints(self):
+    endpoint_map = {}
+    func_app_map = {}
+    for i in self.duration_list:
+      for _, row in i.iterrows():
+        hash_app = row['HashApp']
+        hash_function = row['HashFunction']
+        if len(self.endpoints) > 0:
+          next_endpoint = self.endpoints[0]
+          self.endpoints.pop(0)
+          endpoint_map[hash_function] = next_endpoint
+          func_app_map[hash_function] = hash_app
+        else:
+          break
+      if len(self.endpoints) == 0:
+        break
+
+    return endpoint_map, func_app_map
+
+  # Function to parse traces and generate dataframes
+  def __parseTraces(self, tracedir):
+    # Helper function to check whether a trace should be parsed
+    def shouldParse(trace_name):
+      if not self.filter_days:
+        return True
+      for d in self.filter_days:
+        curr_day = 'd%02d' % d
+        if curr_day in trace_name:
+          return True
+      return False
+
+    all_traces = glob.glob(os.path.join(tracedir, '*.csv'))
+
+    invocations_list = []
+    memory_list = []
+    duration_list = [] 
+    for at in all_traces:
+      # Check whether we should parse the next trace
+      if not shouldParse(at):
+        continue
+
+      print('Now parsing:', os.path.basename(at))
+
+      # Read into DF
+      df = pd.read_csv(at)
+
+      if 'app_memory' in at:
+        # Keep columns: HashApp, AverageAllocatedMb_pct99
+        df = df[['HashApp','AverageAllocatedMb_pct99']]
+        memory_list.append(df)
+      elif 'function_durations' in at:
+        # Keep columns: HashApp, HashFunction, percentile_Average_99
+        df = df[['HashApp', 'HashFunction', 'percentile_Average_99']]
+        duration_list.append(df)
+      elif 'invocations_per_function' in at:
+        # (Easier to drop.) Drop columns: HashOwner, Trigger
+        df = df.drop(['HashOwner', 'Trigger'], axis=1)
+        invocations_list.append(df)
+
+    return invocations_list, memory_list, duration_list
